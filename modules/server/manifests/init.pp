8140579d4a886b2e8ab97e466521e985840c327b
Calvin Zhang <sanping.zhang@alcatel-lucent.com>
2015-01-23 09:20:19 +0000

    clean up

--- modules/server/manifests/init.pp
+++ modules/server/manifests/init.pp
@@ -4,11 +4,11 @@ class server {
 			$files_home = 'puppet:///modules/server'
 		}
 	}
-	file { '/boot/grub/grub.conf':
-		mode => 600,
-		owner => root,
-		group => root,
-		ensure => file,
-		source => "$files_home/grub.conf",
-	}
+#	file { '/boot/grub/grub.conf':
+#		mode => 600,
+#		owner => root,
+#		group => root,
+#		ensure => file,
+#		source => "$files_home/grub.conf",
+#	}
 }
